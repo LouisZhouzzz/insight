4066ecc985ddaf778eab5df35b5ca6c7615c4874
梁栋材 <376638679@qq.com>
2018-07-12 11:18:06 +0800

    echarts实例

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,91 @@
-//index.js
-//获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  onShareAppMessage: function (res) {
+    return {
+      title: 'ECharts 可以在微信小程序中使用啦！',
+      path: '/pages/index/index',
+      success: function () {},
+      fail: function () {}
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  data: {
+    charts: [{
+      id: 'bar',
+      name: '柱状图'
+    }, {
+      id: 'scatter',
+      name: '散点图'
+    }, {
+      id: 'pie',
+      name: '饼图'
+    }, {
+      id: 'line',
+      name: '折线图'
+    }, {
+      id: 'funnel',
+      name: '漏斗图'
+    }, {
+      id: 'gauge',
+      name: '仪表盘'
+    }, {
+      id: 'k',
+      name: 'K 线图'
+    }, {
+      id: 'radar',
+      name: '雷达图'
+    }, {
+      id: 'heatmap',
+      name: '热力图'
+    }, {
+      id: 'tree',
+      name: '树图'
+    }, {
+      id: 'treemap',
+      name: '矩形树图'
+    }, {
+      id: 'sunburst',
+      name: '旭日图'
+    }, {
+      id: 'map',
+      name: '地图'
+    }, {
+      id: 'graph',
+      name: '关系图'
+    }, {
+      id: 'boxplot',
+      name: '箱型图'
+    }, {
+      id: 'parallel',
+      name: '平行坐标图'
+    }, {
+      id: 'sankey',
+      name: '桑基图'
+    }, {
+      id: 'themeRiver',
+      name: '主题河流图'
+    }],
+    chartsWithoutImg: [{
+      id: 'lazyLoad',
+      name: '延迟加载图表'
+    }, {
+      id: 'multiCharts',
+      name: '一个页面中多个图表'
+    }, {
+      id: 'move',
+      name: '页面不阻塞滚动'
+    }, {
+      id: 'saveCanvas',
+      name: '保存 Canvas 到本地文件'
+    }]
+  },
+
+  onReady() {
+  },
+
+  open: function (e) {
+    wx.navigateTo({
+      url: '../' + e.target.dataset.chart.id + '/index'
+    });
   }
-})
+});
